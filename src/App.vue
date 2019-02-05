659dfc003537f5983a3743f3eeb142748e5bfec5
Riley Robertson <jrr8@rice.edu>
2019-02-05 00:14:15 -0500

    Successfully migrated Header component from old portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+/* Make bootstrap globally available */
+@import "assets/bootstrap.min.css";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background: #c9e5ff;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #f2f7ff, #c9e5ff);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #f2f7ff, #c9e5ff);
+  font-family: 'Lato', sans-serif;
+  user-select: none;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="face-cursor py-5 px-3 display-2 thin">{{ displayedMsg + cursor }}</div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: "Riley Robertson",
+      msgInterval: null,
+      cursorInterval: null,
+      msgIdx: 1,
+      cursor: "_"
+    }
+  },
+  computed: {
+    displayedMsg: function() {
+      return this.msg.slice(0, this.msgIdx)
+    }
+  },
+  methods: {
+    toggleCursor: function() {
+      this.cursor = (this.cursor === "_") ? "" : "_";
+    },
+    clearCursor: function() {
+      this.cursor = "";
+      clearInterval(this.cursorInterval);
+    },
+    type: function() {
+      if (this.msgIdx < this.msg.length) {
+        // Add some random spice to the typing effect
+        if (Math.random() < 0.64) {
+          this.msgIdx += 1
+        } else if (this.msgIdx > 1 && Math.random() < 0.25) {
+          this.msgIdx -= 1
+        }
+      } else if (this.msgInterval) {
+        // Done typing. Clear both intervals
+        clearInterval(this.msgInterval);
+        setTimeout(this.clearCursor, 5041)
+      }
+    }
+  },
+  mounted: function() {
+    this.cursorInterval = setInterval(this.toggleCursor, 625);
+    this.msgInterval = setInterval(this.type, 144)
+  }
+}
+</script>
+
+
+<style scoped>
+  .face-cursor {
+    cursor: url('~@/assets/cursor.png') 64 64, crosshair; /* Place hotspot in the middle of 128x128 image */
+  }
+</style>
