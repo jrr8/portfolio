356051fbebbba87e8c7999b49c4b1029faddb476
Riley Robertson <jrr8@rice.edu>
2019-02-05 12:28:20 -0500

    Added s3 deploy package

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? 'http://broccoli-rob.me/'
-    : '/'
+    : '/',
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      region: 'us-east-1',
+      bucket: 'broccoli-rob.me',
+      createBucket: false,
+      staticHosting: false,
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
 }
