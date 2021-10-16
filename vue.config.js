25338f412d4992078274da54c666704cd33a77c9
Riley Robertson <riley@muvve.com>
2021-10-16 14:47:58 -0400

    Update S3 deploy package + config

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,10 @@ module.exports = {
     s3Deploy: {
       registry: undefined,
       awsProfile: 'default',
-      region: 'us-east-1',
+      region: 'us-east-2',
       bucket: 'broccoli-rob.me',
       createBucket: false,
-      staticHosting: false,
+      staticHosting: true,
       assetPath: 'dist',
       assetMatch: '**',
       deployPath: '/',
@@ -18,7 +18,10 @@ module.exports = {
       pwa: false,
       enableCloudfront: false,
       uploadConcurrency: 5,
-      pluginVersion: '3.0.0'
+      pluginVersion: '4.0.0-rc3',
+      overrideEndpoint: false,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html'
     }
   }
 }
